7820c119e23b4bc4cb0869803d48d412275f1982
Abhisd <abhishek.dawange@gmail.com>
2020-07-29 11:38:43 +0530

    spring example of injecting interface

---
+++ springcoreAdvanced/src/main/java/com/abhishek/spring/springcoreAdvanced/injecting/interfaces/OrderDAOImpl.java
@@ -0,0 +1,11 @@
+package com.abhishek.spring.springcoreAdvanced.injecting.interfaces;
+
+public class OrderDAOImpl implements OrderDAO {
+
+	public void createOrder() {
+		// TODO Auto-generated method stub
+		System.out.println("inside order dao");
+
+	}
+
+}
